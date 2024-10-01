7e8f82c965372ea11e0d901af28fcc7197f81cda
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-10-01 15:04:00 +0530

    My first commit

---
+++ hellopython.py
@@ -0,0 +1,12 @@
+
+student=input("Enter the student name")
+number= int(input("enter the roll number:"))
+number=number+1
+print("Name of the student"),student
+print("Roll number:",number)
+CG= int(input("CGPA of the student:"))
+CG=CG+1
+print("CGPA of the student",CG)
+
+
+
